767c2830db0b70f4b35b85a5de1235864e4447f6
zchen8-hub <zchen8@wpi.edu>
2020-04-27 00:03:22 -0400

    signin update

--- src/view/SignIn.js
+++ src/view/SignIn.js
@@ -11,110 +11,142 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Container from '@material-ui/core/Container';
+import { Redirect } from 'react-router'
+
 import CopyRight from "../components/copyright";
 import useStyles from "../components/useStyles";
 import userApi from "../api/user"
 
-class SignIn extends Component{
-    constructor(props){
+class SignIn extends Component {
+    constructor(props) {
         super(props);
-        this.state ={
-            username:'',
-            password:''
+        this.state = {
+            username: '',
+            password: '',
+            userId: '',
+            redirect: false
         }
         this.onChange = this.onChange.bind(this);
         this.handleSignIn = this.handleSignIn.bind(this);
     }
 
-    onChange(event){
-        this.setState({[event.target.name]: event.target.value});
+    onChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value });
     }
 
-    handleSignIn(){
-        const {username, password} = this.state
+    handleSignIn() {
+        const { username, password } = this.state
 
         const user = {
             username,
             password
         }
 
-        userApi.login(user,(response)=>{
-            window.open("/selectProject")
+        debugger;
+        userApi.login(user, (response) => {
+            this.setState({ userId: response.uid });
+            console.log("success");
+            this.setState({ redirect: true });
         })
 
     }
 
     render() {
         const classes = useStyles;
-    
-        return (
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <div className={classes.paper}>
-                    <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <form className={classes.form} noValidate>
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="username"
-                            label="UserName"
-                            name="username"
-                            value ={this.username}
-                            onChange = {this.onChange}
-                            autoFocus
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            value ={this.password}
-                            onChange = {this.onChange}
-                            autoComplete="current-password"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="Remember me"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                            onClick = {this.handleSignIn}
-                        >
-                            Sign In
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="./SignUp" variant="body2">
-                                    {"Don't have an account? Sign Up"}
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </form>
-                </div>
-                <Box mt={8}>
-                    <CopyRight />
-                </Box>
-            </Container>
+
+        if (this.state.redirect) {
+            return <Redirect to = {
+                {
+                    pathname: '/Project',
+                    state: {
+                        userId: this.state.userId,
+                        username: this.state.username
+                    }
+                }
+            }
+            />;
+        }
+
+        return ( <
+            Container component = "main"
+            maxWidth = "xs" >
+            <
+            CssBaseline / >
+            <
+            div className = { classes.paper } >
+            <
+            Avatar className = { classes.avatar } >
+            <
+            LockOutlinedIcon / >
+            <
+            /Avatar> <
+            Typography component = "h1"
+            variant = "h5" >
+            Sign in
+            <
+            /Typography> <
+            form className = { classes.form }
+            noValidate >
+            <
+            TextField variant = "outlined"
+            margin = "normal"
+            required fullWidth id = "username"
+            label = "UserName"
+            name = "username"
+            value = { this.username }
+            onChange = { this.onChange }
+            autoFocus /
+            >
+            <
+            TextField variant = "outlined"
+            margin = "normal"
+            required fullWidth name = "password"
+            label = "Password"
+            type = "password"
+            id = "password"
+            value = { this.password }
+            onChange = { this.onChange }
+            autoComplete = "current-password" /
+            >
+            <
+            FormControlLabel control = { < Checkbox value = "remember"
+                color = "primary" / > }
+            label = "Remember me" /
+            >
+            <
+            Link type = "submit"
+            fullWidth variant = "contained"
+            color = "primary"
+            className = { classes.submit }
+            onClick = { this.handleSignIn } >
+            Sign In <
+            /Link> <
+            Grid container >
+            <
+            Grid item xs >
+            <
+            Link href = "#"
+            variant = "body2" >
+            Forgot password ?
+            <
+            /Link> <
+            /Grid> <
+            Grid item >
+            <
+            Link href = "./SignUp"
+            variant = "body2" > { "Don't have an account? Sign Up" } <
+            /Link> <
+            /Grid> <
+            /Grid> <
+            /form> <
+            /div> <
+            Box mt = { 8 } >
+            <
+            CopyRight / >
+            <
+            /Box> <
+            /Container>
         );
     }
 }
